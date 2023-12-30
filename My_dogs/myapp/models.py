23bdd2e7b66814b8cf1b0d4f2375abf846816d49
vadim <korolkovpoland@gmail.com>
2023-12-30 22:20:42 +0200

    'add_base'

--- My_dogs/myapp/models.py
+++ My_dogs/myapp/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField( max_length=50)
     price = models.IntegerField()
+    phone = models.IntegerField(null=True)
     description = models.CharField(max_length=200)
-    text = models.CharField(max_length=200)
+    text = models.CharField(default='Ваше значение по умолчанию', max_length=255)
     images = models.ImageField(blank=True, upload_to='images')
     mp3_file = models.FileField(upload_to='mp3_files')
     
